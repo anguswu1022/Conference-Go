f28be6cbecb766da3f121be9ef46f9a34d415ad0
Angus Wu <anguswu1022@ucla.edu>
2023-02-07 09:01:37 -0800

    Finished assignment

--- events/api_views.py
+++ events/api_views.py
@@ -97,7 +97,14 @@ def api_list_locations(request):
         ]
     }
     """
-    return JsonResponse({})
+    locations = [
+        {
+            "name": loc.name,
+            "href": loc.get_api_url(),
+        }
+        for loc in Location.objects.all()
+    ]
+    return JsonResponse({"locations": locations})
 
 
 def api_show_location(request, id):
@@ -117,4 +124,14 @@ def api_show_location(request, id):
         "state": the two-letter abbreviation for the state,
     }
     """
-    return JsonResponse({})
+    location = Location.objects.get(id=id)
+    return JsonResponse(
+        {
+            "name": location.name,
+            "city": location.city,
+            "room_count": location.room_count,
+            "created": location.created,
+            "updated": location.updated,
+            "state": location.state.name,
+        }
+    )
